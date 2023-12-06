2741a403010d562fe3d841e201cd95db3e255db5
soerenssch <soeren.schlisske@web.de>
2023-12-06 22:40:27 +0100

    update

--- Assignment.py
+++ Assignment.py
@@ -1,20 +1,5 @@
 import streamlit as st
 
 
-element = st.empty()
-element.line_chart(...)
-element.text_input(...)  # Replaces previous.
+st.write('Hello')
 
-# Insert out of order.
-elements = st.container()
-elements.line_chart(...)
-st.write("Hello")
-elements.text_input(...)  # Appears above "Hello".
-
-st.help(pandas.DataFrame)
-st.get_option(key)
-st.set_option(key, value)
-st.set_page_config(layout='wide')
-st.experimental_show(objects)
-st.experimental_get_query_params()
-st.experimental_set_query_params(**params)
