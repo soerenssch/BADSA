653fe2d5e05e27ae365976472bfe06f58b05c48c
soerenssch <soeren.schlisske@web.de>
2023-12-06 22:43:30 +0100

    update

--- Assignment.py
+++ Assignment.py
@@ -1,5 +1,28 @@
 import streamlit as st
 
 
-st.write('Hello')
+st.set_page_config(
+    page_title="Sentiment-Analyse",
+    page_icon="📊",
+    layout="wide",
+    initial_sidebar_state="expanded",    
+)
 
+st.markdown(
+    """
+    <style>
+    .footer {
+        position: fixed;
+        bottom: 0;
+        right: 0;
+        font-size: 12px;
+        padding: 5px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+st.markdown(
+    '<div class="footer">© 2023 Capstone Gruppe Research. All rights reserved.</div>',
+    unsafe_allow_html=True,
+)
