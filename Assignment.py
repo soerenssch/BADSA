11b7f656a614a915807ca6ece450072c3e5a1529
soerenssch <soeren.schlisske@web.de>
2023-12-07 13:26:51 +0100

    OpenWeather

--- Assignment.py
+++ Assignment.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import requests
 import matplotlib.pyplot as plt
 
 ## Graphic layout streamlit
@@ -28,16 +29,38 @@ st.markdown(
 st.header('Hello')
 
 ## Sidebar
-UploadData = 'Upload data'
-Test2 = 'Test2'
+WeatherData = 'Weather Data'
+Visualisation = 'Visualisation'
 
-a = st.sidebar.radio('Choose:',[UploadData, Test2])
+a = st.sidebar.radio('Choose:',[WeatherData, Visualisation])
 
 ## Upload data to analyse
-if a == UploadData:
+if a == WeatherData:
     file = st.file_uploader("Upload your data:", type=["csv"])
 
-if a == Test2:
+
+    key = st.text_input('Enter your OpenWeather API key:')
+    url = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid={key}'
+
+    response = requests.get(url)
+
+        if response.status_code == 200:
+            # Parse the JSON response
+            weather_data = response.json()
+
+        # Print current weather conditions
+        st.write(f"Current weather conditions for London:")
+        st.write("------------------------------------")
+        st.write(f"Temperature: {weather_data['main']['temp']}°C")
+        st.write(f"Humidity: {weather_data['main']['humidity']:.0f}%")
+        st.write(f"Pressure: {weather_data['main']['pressure']} hPa")
+        st.write(f"Description: {weather_data['weather'][0]['description']}")
+    else:
+        st.error(f"Error retrieving weather data: {response.status_code}")
+
+
+
+if a == Visualisation:
 
     x = np.linspace(0, 10, 200) 
     y = 2 * x + 1 + np.random.normal(0, 2, size=len(x))
